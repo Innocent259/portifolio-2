9523a2eb7624bb3c621d0c8013933566af650e5c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-03-25 14:39:03 +0200

    Display Intro component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Intro from "./components/Intro";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Portolio
+    <main className="flex min-h-screen flex-col items-center gap-6 p-24">
+      <Intro />
     </main>
   );
 }
