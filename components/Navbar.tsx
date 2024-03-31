56620cf2678d9578b9db8bae03dd1f627df1f852
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-03-31 20:50:46 +0200

    Format code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion"
 import { MdOutlineMenu, MdClose } from "react-icons/md";
-import { links } from "@/lib/data"; 
+import { links } from "@/lib/data";
 
 const Navbar: React.FC = () => {
 
@@ -32,27 +32,27 @@ const Navbar: React.FC = () => {
         )}
         {
           menu && (
-            <motion.div 
-            className="flex flex-col gap-4 bg-[#ddd] p-4 rounded-sm"
-            initial={{ x: -50, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
+            <motion.div
+              className="flex flex-col gap-4 bg-[#ddd] p-4 rounded-sm"
+              initial={{ x: -50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.1
+              }}
             >{
-              links.map(link => <Link key={link.hash} href={link.hash}>{link.name}</Link>)
-            }
+                links.map(link => <Link key={link.hash} href={link.hash}>{link.name}</Link>)
+              }
             </motion.div>
           )
         }
       </div>
-      <motion.div 
-      className="hidden md:flex gap-4 bg-[#ddd] w-fit mx-auto p-4 rounded-full"
-      initial={{ y: -50, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      >
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/skills">Skills</Link>
-        <Link href="/experience">Experience</Link>
-        <Link href="/contact">Contact</Link>
+      <motion.div
+        className="hidden md:flex gap-4 bg-[#ddd] w-fit mx-auto p-4 rounded-full"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >{
+          links.map(link => <Link key={link.hash} href={link.hash}>{link.name}</Link>)
+        }
       </motion.div>
     </nav>
   )
