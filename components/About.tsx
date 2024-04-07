198ab5b9876bfac1e2911e3612c54df6ba7e2488
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-07 23:38:25 +0200

    Create About header

--- components/About.tsx
+++ components/About.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-black'>
-      This is about page
+    <div className='text-center flex flex-col gap-2'>
+        <h1 className='text-xl font-bold'>About</h1>
+      After my university studies in Mechanical engineering 
     </div>
   )
 }
