31e2887593a7b79bbc2f5f1b235f8bf313b38060
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-08 14:11:08 +0200

    Add temporary height

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Intro from "@/components/Intro";
 
 export default function Home() { 
   return (
-    <main className="flex flex-col items-center gap-6 p-12">
+    <main className="flex flex-col items-center gap-6 p-12 h-[66rem]">
       <Intro />
       <About />
     </main>
